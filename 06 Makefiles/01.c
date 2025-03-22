9ca951f8778b419660f75bae8f4229046d875a20
[Avinash Dubey] <avinashdubeyg2001@gmail.com>
2025-03-22 12:58:22 +0530

    Created first commit

---
+++ 06 Makefiles/01.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello Shivam!\n");
+    return 0;
+}
---
+++ 06 Makefiles/03.cu
@@ -0,0 +1,12 @@
+#include <cuda_runtime.h>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "boo!" << endl;  
+
+    return 0;
+}
